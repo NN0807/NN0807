5e5667bd5dac95f1c8bf7942083d73c7ac5fe747
IDaina <2210092@ecc.ac.jp>
2024-12-23 15:57:21 +0900

    板本(最新版)　InputSystem追加

---
+++ Assets/Scripts/Character/CharacterOperation.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; // Input System関連のAPI
+
+public class CharacterOperation : MonoBehaviour
+{
+    private HakopanControls inputActions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        inputActions = new HakopanControls();
+        inputActions.Enable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(inputActions.Player.Fire.triggered)
+        {
+            Debug.Log("ファイヤー");
+        }
+
+        if (inputActions.Player.Pause.triggered)
+        {
+            Debug.Log("ポーズ");
+        }
+    }
+}
