6c93a12134bb65932ae4dd6ece5066a4983a8d50
NN0807 <machawarabimoti@icloud.com>
2025-01-29 14:48:49 +0900

    ファイアステージ以外のナビメッシュサーフェイス生成処理追加

--- Assets/Scripts/Stages/Gimmick/CubeManager.cs
+++ Assets/Scripts/Stages/Gimmick/CubeManager.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using StageCommon;
+using UnityEngine.AI;
+using Unity.AI.Navigation;
 
 public class CubeManager : MonoBehaviour
 {
@@ -12,6 +14,8 @@ public class CubeManager : MonoBehaviour
 
     // 生成されたキャラクターパーツ登録用リスト
     private List<IGimmickPart> _characterParts = new List<IGimmickPart>();
+
+    bool a = true;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +29,18 @@ public class CubeManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(a)
+        {
+            CreateNavMesh();
+            a = !a;
+        }
+    }
+
+    private void CreateNavMesh()
+    {        
+        // ナビメッシュサーフェイス生成
+        NavMeshSurface navMeshSurface = gameObject.AddComponent<NavMeshSurface>();
+        navMeshSurface.layerMask = LayerMask.GetMask("Stage");
+        navMeshSurface.BuildNavMesh();
     }
 }
